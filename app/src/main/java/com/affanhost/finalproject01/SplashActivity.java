b0ae69e4db4b506ca808f440f1c0f3e048f46782
Rehan Ahmad Khan <rehankhan.mfdhiee@gmail.com>
2022-06-14 23:57:06 +0500

    LottiFiles Dependencies updated and Lottifile view added to the SplashActivity Layout

---
+++ app/src/main/java/com/affanhost/finalproject01/SplashActivity.java
@@ -0,0 +1,14 @@
+package com.affanhost.finalproject01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
