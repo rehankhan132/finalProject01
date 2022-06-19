b982af6e6f3a8885fc25c257199cb375980388ae
Rehan Ahmad Khan <rehankhan.mfdhiee@gmail.com>
2022-06-19 20:44:23 +0500

    Login and Signup Activities are created.

---
+++ app/src/main/java/com/affanhost/finalproject01/LoginActivity.java
@@ -0,0 +1,37 @@
+package com.affanhost.finalproject01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        getSupportActionBar().hide();
+
+        TextView signUpActivity = findViewById(R.id.txtViewSignUpFromLogin);
+        signUpActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(LoginActivity.this,SignUpActivity.class);
+                startActivity(i);
+            }
+        });
+
+        TextView homeActivity = findViewById(R.id.txtViewHomeFromLogin);
+        homeActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(LoginActivity.this,MainActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/affanhost/finalproject01/SignUpActivity.java
@@ -0,0 +1,37 @@
+package com.affanhost.finalproject01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class SignUpActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+        getSupportActionBar().hide();
+
+        TextView loginActivity = findViewById(R.id.txtViewLoginFromSignUp);
+        loginActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(SignUpActivity.this,LoginActivity.class);
+                startActivity(i);
+            }
+        });
+
+        TextView homeActivity = findViewById(R.id.txtViewHomeFromSignup);
+        homeActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(SignUpActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
