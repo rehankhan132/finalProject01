3a6df28bf46f37efb5e389a11e3064d91f88826a
Rehan Ahmad Khan <rehankhan.mfdhiee@gmail.com>
2022-06-26 23:44:00 +0500

    UserProfile Process has been successful..

--- app/src/main/java/com/affanhost/finalproject01/ProfileActivity.java
+++ app/src/main/java/com/affanhost/finalproject01/ProfileActivity.java
@@ -1,10 +1,31 @@
 package com.affanhost.finalproject01;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    private FirebaseUser user;
+    private DatabaseReference reference;
+    private String userID;
+
+    private Button btnLogout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +33,50 @@ public class ProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
 
         getSupportActionBar().hide();
+
+        btnLogout = findViewById(R.id.btnLogout);
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(ProfileActivity.this,MainActivity.class));
+            }
+        });
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference("Users");
+        userID = user.getUid();
+        TextView tvGreetings = findViewById(R.id.tvGreetings);
+        TextView tvEmail = findViewById(R.id.tvEmail);
+        TextView tvName = findViewById(R.id.tvName);
+        TextView tvUsername = findViewById(R.id.tvUsername);
+
+        reference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                User userProfile = snapshot.getValue(User.class);
+
+                if(userProfile != null){
+
+                    String fullname = userProfile.fullname;
+                    String email = userProfile.email;
+                    String username = userProfile.username;
+
+                    tvGreetings.setText("Welcome: "+ fullname+ "!");
+                    tvEmail.setText(email);
+                    tvName.setText(fullname);
+                    tvUsername.setText(username);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+                Toast.makeText(ProfileActivity.this,"Something Wrong Happened!", Toast.LENGTH_LONG).show();
+
+            }
+        });
     }
 }
